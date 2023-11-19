80e43b0965ab1770155abf3efab7670378ee4cd3
odilo <odilomurindahabi@gmail.com>
2023-11-19 15:25:22 +0200

    create authSlice

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,61 @@
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link } from 'react-router-dom'
-
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-  const expand = 'md'
+  const expand = 'md';
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleToggleOffcanvas = () => {
+    setShowOffcanvas((prevShowOffcanvas) => !prevShowOffcanvas);
+  };
+
+  const handleNavItemClick = () => {
+    setShowOffcanvas(false); // Close the Offcanvas when a navigation link is clicked
+  };
+
   return (
     <>
-
-        <Navbar expand={expand} className="bg-body-tertiary">
-          <Container fluid>
-            <Navbar.Brand as={Link} to='/'>Todo App</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link as={Link} to='/' className='btn btn-light'> Home</Nav.Link>
-                  <Nav.Link as={Link} to='/' className='btn btn-light'>My todos</Nav.Link>
-                  <Nav.Link as={Link} to='/sign-in' className='btn btn-light'>Login</Nav.Link>
-                  <Nav.Link as={Link} to='/sign-up' className='btn btn-light'>Register</Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+      <Navbar expand={expand} className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand as={NavLink} to="/" activeClassName="active">
+            Todo App
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={handleToggleOffcanvas} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+            show={showOffcanvas}
+            onHide={() => setShowOffcanvas(false)}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={NavLink} to="/" activeClassName="active" className="btn btn-light" onClick={handleNavItemClick}>
+                  Home
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/my-todos" activeClassName="active" className="btn btn-light" onClick={handleNavItemClick}>
+                  My Todos
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/sign-in" activeClassName="active" className="btn btn-light" onClick={handleNavItemClick}>
+                  Login
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/sign-up" activeClassName="active" className="btn btn-light" onClick={handleNavItemClick}>
+                  Register
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </>
   );
 }
---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+const initialState = {
+  user: null,
+  isLoading: false,
+  isSuccess: false,
+  isError:false
+}
+
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    
+  }
+})
+
+export default authSlice.reducer
