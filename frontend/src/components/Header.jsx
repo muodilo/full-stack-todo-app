a9547d015e085532cd3c7ef2163d46a9238262b3
odilo <odilomurindahabi@gmail.com>
2023-11-19 14:41:08 +0200

    create register page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,9 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 function Header() {
   const expand = 'md'
@@ -28,10 +26,10 @@ function Header() {
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                  <Nav.Link as={Link} to='/'>My todos</Nav.Link>
-                  <Nav.Link as={Link} to='/sign-in'>Login</Nav.Link>
-                  <Nav.Link as={Link} to='/sign-up'>Register</Nav.Link>
+                  <Nav.Link as={Link} to='/' className='btn btn-light'> Home</Nav.Link>
+                  <Nav.Link as={Link} to='/' className='btn btn-light'>My todos</Nav.Link>
+                  <Nav.Link as={Link} to='/sign-in' className='btn btn-light'>Login</Nav.Link>
+                  <Nav.Link as={Link} to='/sign-up' className='btn btn-light'>Register</Nav.Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,43 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='container-fluid main-form bg-body-secondary'>
+      <div className="register p-3 rounded">
+        <section className="header text-center pb-4">
+          <h1>Register</h1>
+          <hr />
+        </section>
+        <div className="form">
+          <form>
+            <input
+              type="text"
+              className="form-control mb-3"
+              placeholder='Enter your name'
+              
+            />
+            <input
+              type="email"
+              className="form-control mb-3"
+              placeholder='Enter your email'
+
+            />
+            <input
+              type="password"
+              className="form-control mb-3"
+              placeholder='Create password'
+
+            />
+            <input
+              type="password"
+              className="form-control mb-3"
+              placeholder='Confirm password'
+
+            />
+            <button type="submit" className='btn btn-primary w-100'>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
