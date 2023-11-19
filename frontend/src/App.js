80817175250893a7e016d559008873dde28d7ed9
odilo <odilomurindahabi@gmail.com>
2023-11-19 13:59:43 +0200

    add create Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Home from './pages/Home';
+import Register from './pages/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      my app
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={ <Home/>} />
+        <Route path='/sign-up' element={ <Register/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import {Link} from 'react-router-dom'
+
+function Header() {
+  const expand = 'md'
+  return (
+    <>
+
+        <Navbar expand={expand} className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand as={Link} to='/'>Todo App</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                  <Nav.Link as={Link} to='/'>My todos</Nav.Link>
+                  <Nav.Link as={Link} to='/sign-in'>Login</Nav.Link>
+                  <Nav.Link as={Link} to='/sign-up'>Register</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+    </>
+  );
+}
+
+export default Header;
