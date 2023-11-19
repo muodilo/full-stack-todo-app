afa20e4bf2fbd408f8bddea6a153ba6b6c3f32a8
odilo <odilomurindahabi@gmail.com>
2023-11-19 03:46:25 +0200

    add errorHandlerMiddleware

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const connectDB = require('./config/db')
+const { errorHandler }= require('./middleware/errorHandlerMiddleware')
 
 const PORT = process.env.PORT
 connectDB()
 
 const app = express()
-app.use('/api/users',require('./routes/userRoutes'))
 
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}...`))
